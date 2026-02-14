b3ae2fb4e500f8d3b8961e2cd28453ba814441e4
SICKFL0W <filipmsindelar@gmail.com>
2026-02-14 03:00:27 +0100

    Final Build V1.2: Fixed fonts, sounds and Info.plist

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// DŮLEŽITÉ: Registrujeme to jako 'main', protože to iOS vyžaduje
+AppRegistry.registerComponent('main', () => App);
